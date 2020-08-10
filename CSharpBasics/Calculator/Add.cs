8ac58612997d3c79a13b69cd125ada4a2f9114fd
Om Prakash <omprakash.gupta@outlook.com>
2020-08-10 20:25:51 +0530

    Commit the Add.cs

---
+++ CSharpBasics/Calculator/Add.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpBasics.Calculator
+{
+    public class Add
+    {
+        private int Addition(int a, int b)
+        {
+            return a + b;
+        }
+    }    
+}
